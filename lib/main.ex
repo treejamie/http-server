f3def0512556dde80395a501dea7d4b6039a9f87
Tree Jamie <me@treejamie.com>
2026-04-22 20:36:52 +0100

    feat: return 200 response

--- lib/main.ex
+++ lib/main.ex
@@ -1,18 +1,20 @@
 defmodule Server do
   use Application
+  require Logger
 
   def start(_type, _args) do
+    Logger.info("Starting server on http://127.0.0.1:4221")
     Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
   end
 
   def listen() do
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    IO.puts("Logs from your program will appear here!")
-
-    # Since the tester restarts your program quite often, setting SO_REUSEADDR
-    # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
-    {:ok, _client} = :gen_tcp.accept(socket)
+
+    with {:ok, client} <- :gen_tcp.accept(socket) do
+      :gen_tcp.send(client, "HTTP/1.1 200 OK\r\n\r\n")
+    else
+      err -> err
+    end
   end
 
   def main(_args) do
