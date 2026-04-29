2824b7c30b19a1577861072a3c77639e91ce0fce
treejamie <me@treejamie.com>
2026-04-29 15:28:28 +0100

    feat: added in credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -9,7 +9,14 @@ defmodule App.MixProject do
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      aliases: aliases(),
       escript: [main_module: Server]
+    ]
+  end
+
+  defp aliases do
+    [
+      check: ["format --check-formatted", "credo --strict", "dialyzer", "test"]
     ]
   end
 
@@ -24,8 +31,8 @@ defmodule App.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
